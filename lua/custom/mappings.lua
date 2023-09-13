685346928c57d2405c3824bb020f075ffd984e44
Slavik Slinyuk <slinyuk.v@hexide-digital.com>
2023-09-13 08:56:57 +0300

    add: new mappings for split
    Added new mappings for split view and force quit keybind

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -5,7 +5,12 @@ M.general = {
   n = {
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
     ["<C-p>"] = { "<cmd> Telescope git_files<cr>", "Find in Git files"},
-   ["<C-t>"] = { "<cmd> ToggleTerm<cr>", "Open terminal"},
+    ["<C-t>"] = { "<cmd> ToggleTerm<cr>", "Open terminal"},
+    ["<leader>sh"] = {"<cmd> split<cr>", "Split horizontal"},
+    ["<leader>sv"] = {"<cmd> vsplit<cr>", "Split vertical"},
+    ["<leader>sc"] = {"<cmd> close<cr>", "Close split"},
+    ["<leader>qqq"] = {"<cmd> qa!<cr>", "Force quit"},
+    ["<leader>gd"] = {vim.lsp.buf.definition, "Go to definition"},
   },
 }
 
